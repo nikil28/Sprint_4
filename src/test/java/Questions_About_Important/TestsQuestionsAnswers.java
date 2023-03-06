67044d6e8c1cdb00eef72362cb2a2359aa5d7099
Akexey Nikitin <anikitin@rzdit.ru>
2023-03-06 19:37:38 +0300

    1. Учел все комментарии

--- src/test/java/Questions_About_Important/TestsQuestionsAnswers.java
+++ src/test/java/Questions_About_Important/TestsQuestionsAnswers.java
@@ -1,6 +1,6 @@
 package Questions_About_Important;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
@@ -19,6 +19,9 @@ import org.junit.runners.Parameterized.Parameters;
 import java.util.Arrays;
 import java.util.Collection;
 
+import org.junit.Before;
+
+
 @RunWith(Parameterized.class)
 public class TestsQuestionsAnswers { // тут тесты на вопросы-ответы
     private final String expectedString;
@@ -26,21 +29,27 @@ public class TestsQuestionsAnswers { // тут тесты на вопросы-о
     private final By selector;
 
     MainPage mainPage;
+    static WebDriver driver;
 
     public TestsQuestionsAnswers(By selector, String expectedString, String panelID) {
         this.expectedString = expectedString;
         this.panelID = panelID;
         this.selector = selector;
     }
+    @Before
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS); // добавили централизованно ожидание в 5 секунд
+        mainPage = new MainPage(driver); // инициализация класса MainPage
+        mainPage.open();
+        WebElement element = driver.findElement(By.id("accordion__heading-5")); // Прокрути страницу
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView();", element);
+    }
 
     @Parameters
     public static Collection<Object[]> data() {
-        WebDriver driver;
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS); // добавили ожидание в 5 секунд
-        MainPage mainPage = new MainPage(driver);
-        driver.quit();
+         MainPage mainPage = new MainPage(driver);
         return Arrays.asList(new Object[][]{
                 {mainPage.paymentСostButton,"Сутки — 400 рублей. Оплата курьеру — наличными или картой.","accordion__panel-0"},
                 {mainPage.severalScooters, "Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим.","accordion__panel-1"},
@@ -55,17 +64,12 @@ public class TestsQuestionsAnswers { // тут тесты на вопросы-о
 
     @Test
     public void check(){
-        WebDriver driver;
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        mainPage = new MainPage(driver);
-        mainPage.open();
-        WebElement element = driver.findElement(By.id("accordion__heading-5")); // Прокрути страницу
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView();", element);
         mainPage.clickAnObject(selector);
         assertEquals(expectedString, driver.findElement(By.id(panelID)).getText());
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        driver.quit();
     }
+    @After // после теста
+    public void tearDown(){ //метод для закрытия браузера по завершению теста
+        driver.quit(); //  закрыть браузер
+    }
+
 }
